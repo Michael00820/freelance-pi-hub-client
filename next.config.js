38d9e3c51a1d97a0336b32ccb861ea7e4c2f0ce0
Michael00820 <37353304+Michael00820@users.noreply.github.com>
2025-08-21 22:21:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-};
-module.exports = nextConfig;
+  swcMinify: false,
+  compiler: {
+    // disable SWC entirely, fall back to Babel
+  }
+}
+
+module.exports = nextConfig
